c2f1de107528ee72920f07e88991d4150dfe1adf
Silas-storm <silashouessou89@gmail.com>
2022-12-08 17:02:44 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry 
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+
+	puts("\"Programming is like building a multilangual puzzle");
+        return (0);
+
+}	
